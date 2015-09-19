9305c12657d67b88a8ca05ac31f860a675900e0f
Tallpockets <kentjolly@Kents-Mac-Pro.local>
2015-09-19 12:19:13 -0700

    cleaning up script a bit

--- Assets/Scripts/PlaceObject.cs
+++ Assets/Scripts/PlaceObject.cs
@@ -22,29 +22,16 @@ public class PlaceObject : MonoBehaviour {
 				Collider c = rayHit.collider;
 				if(c.gameObject.tag == "Terrain")
 				{
-					MeshFilter mf = c.gameObject.GetComponent<MeshFilter>();
 					MeshRenderer mr = c.gameObject.GetComponent<MeshRenderer>();
-					//Bounds b = mf.mesh.bounds;
 					Bounds br = mr.bounds;
-					//Debug.Log("Filter bounds : " + b);
-					//Debug.Log("Renderder bounds.size : " + br.size);
-					//Debug.Log("HitPoint is : " + rayHit.point);
 					Vector3 p = rayHit.point;
-					//length, height, depth
-					//??, 20,20
-					//float length = p.x / (br.size.x/mGridSize.x);
-					//float height = p.y / (br.size.y/mGridSize.y);
-					//float depth = p.z / (br.size.z/mGridSize.z);
-
 					float length = Mathf.Floor(p.x/(br.size.x/mGridSize.x));
-					float height = Mathf.Floor(p.y/(br.size.x/mGridSize.x));
-					float depth = Mathf.Floor(p.z/(br.size.x/mGridSize.x));
-					Vector3 finalPos = new Vector3(length/2,height/2,depth/2);
-
-					Vector3 offset = new Vector3(0.25f,0.25f,0.25f);
+					float height = Mathf.Floor(p.y/(br.size.y/mGridSize.y));
+					float depth = Mathf.Floor(p.z/(br.size.z/mGridSize.z));
+					Vector3 finalPos = new Vector3(length * (br.size.x/mGridSize.x),height* (br.size.y/mGridSize.y),depth * (br.size.z/mGridSize.z));
+					Vector3 offset = new Vector3((br.size.x/mGridSize.x)/2f,(br.size.y/mGridSize.y)/2f,(br.size.z/mGridSize.z)/2f);
 					finalPos = finalPos + offset;
 
-					//Debug.Log("rayhit: " + p + "grid : " + finalPos);
 					Instantiate(mObjectToPlace, finalPos, new Quaternion(0,0,0,0));
 				} else if(c.gameObject.tag == "PlacedObject")
 				{
